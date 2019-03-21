c7a2a613e42446d33355c64b2b632194c09b7225
Stroupe79 <cstroupe79@gmail.com>
2019-03-21 13:58:24 -0500

    Getting started. Added style to get going

---
+++ assets/trivia.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    var timer = 30;
+   
+   // Qs and As arrays
+    var questions = [
+        "What was the name of Dwight's farm?",
+        "During diversity training day, what race was Stanley's card?",
+        "Which one of the actors for these characters is a real life famous musician?",
+        "Who does Angela name her son after?",
+        "What is the name of the security guard?",
+        "Who said 'Bankruptcy is natures do-ver.'?",
+        "What college did Andy attend?",
+        "What is Micheal's favorite pizza joint in New York?",
+        "Who dressed as 'The Joker' for Halloween?",
+        "What was did Micheal give his 'Scotts Tots' in place of paid tuition to college?",
+        "Which employee did Michael hit with his car?"
+    ]
+   
+   var answers = [
+       ["Schrute Farms", "Beets Unlimited", "Schrute and Sons", "It didn't have a name"]
+       ["Italian", "Indian", "Black", "Asian"],
+       ["Meredith", "Kevin", "Creed", "Angela"],
+       ["Her Father", "Dwight's Father", "Her Cousin", "Her Cat"],
+       ["Hank", "Stanley", "Roger", "They never say"],
+       ["Creed", "Micheal", "Kevin", "Dwight"],
+       ["Yale", "Dartmouth", "Cornell", "Penn State"],
+       ["Luigis", "Sbarro", "Pizza Hut", "Tony's Pie Company"],
+       ["Creed", "Kevin", "Dwight", "All three"],
+       ["Gift Cards to Chilis", "Dunder Mifflin Shirts", "Printer Cartidges", "Laptop Batteries"],
+       ["Phyllis", "Oscar", "Meredith", "Ryan"],
+                           
+
+   ]
+   
+    //Timer function   
+    // $("#timer").text(timer);
+
+    // setInterval(function () {
+    //     timer--;
+    //     if (timer <= 0) {
+    //         alert("Times up")
+    //     }
+    // });
+
+    //correct answer function
+
+    //incorrect answer function
+
+    //display gif on correct
+
+    
+
+
+
+
+
+});
