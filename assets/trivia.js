ad13244e221f7dfd8133ccd0fcb07a277f8aebd7
Stroupe79 <cstroupe79@gmail.com>
2019-03-21 17:26:22 -0500

    Working on DOM for questions and answers

--- assets/trivia.js
+++ assets/trivia.js
@@ -34,6 +34,9 @@ $(document).ready(function () {
    var answerKey = [0, 2, 2, 3, 0, 0, 2, 1, 3, 3, 1]
 
    var currentQuestion = [];
+
+$("#questions").text(questions[0]);
+$("#answers").text(answers[0[0]]);
    
     //Timer function   
     // $("#timer").text(timer);
